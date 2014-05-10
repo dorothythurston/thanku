bb5ed1a25ba8005af201121782daf342e68d010c
dorothy <dorothy.thur@gmail.com>
2014-05-10 17:39:48 -0400

    Add monban

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Thankyou::Application.routes.draw do
+  resource :dashboard, only: [:show]
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   root 'homes#show'
 end
